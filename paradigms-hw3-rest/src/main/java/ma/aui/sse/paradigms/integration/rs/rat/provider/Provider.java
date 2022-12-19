ad1ef7b6cc7f6b1e11587bbdf4d168281f74721a
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-19 20:36:28 +0100

    Update Provider.java

--- paradigms-hw3-rest/src/main/java/ma/aui/sse/paradigms/integration/rs/rat/provider/Provider.java
+++ paradigms-hw3-rest/src/main/java/ma/aui/sse/paradigms/integration/rs/rat/provider/Provider.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Provider {
 
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(Provider.class, args);
     }
     
