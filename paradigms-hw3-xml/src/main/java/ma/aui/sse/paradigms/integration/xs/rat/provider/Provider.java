290d5e7c2d54b181c869add2853b78e4be15d48b
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-19 20:35:52 +0100

    Update Provider.java

--- paradigms-hw3-xml/src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/Provider.java
+++ paradigms-hw3-xml/src/main/java/ma/aui/sse/paradigms/integration/xs/rat/provider/Provider.java
@@ -7,7 +7,7 @@ public class Provider {
     private static final String URL = "http://localhost:8080/rat";
 
     public static void main(String[] args) {
-
+        System.setProperty("java.awt.headless", "false");
         RemoteAccessUtility remoteAccessUtility = new RemoteAccessUtility();
         System.out.println("Publishing RemoteAccessUtility Service");
         Endpoint.publish(URL, remoteAccessUtility);
