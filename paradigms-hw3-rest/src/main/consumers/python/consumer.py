b6152027c65c700b9dd6b29a638cff58d4134e1f
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-20 18:53:41 +0100

    Update consumer.py

--- paradigms-hw3-rest/src/main/consumers/python/consumer.py
+++ paradigms-hw3-rest/src/main/consumers/python/consumer.py
@@ -1,10 +1,17 @@
+# Import the RemoteAccessUtilityApi class from the swagger_client module
 from swagger_client import RemoteAccessUtilityApi
+
+# Import the ApiException class from the swagger_client.rest module
 from swagger_client.rest import ApiException
 
+# Create an instance of the RemoteAccessUtilityApi class
 apiInstance = RemoteAccessUtilityApi()
 
+# Call the get_processes method of the apiInstance object and print the result
 print(apiInstance.get_processes())
 
+# Call the screenshot method of the apiInstance object and print the result
 print(apiInstance.screenshot())
 
+# Call the reboot method of the apiInstance object and print the result
 print(apiInstance.reboot())
